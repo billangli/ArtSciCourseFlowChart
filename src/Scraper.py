dc87acdae41116b7612dd27bd1fc6c1d6aca02fa
Bill Li <libill12@hotmail.com>
2017-07-27 17:45:01 -0400

    Updated URL to a complete calendar

--- src/Scraper.py
+++ src/Scraper.py
@@ -11,13 +11,15 @@ import requests
 
 ### Getting data from the URL
 """tree will contain the entire HMTL file in a tree format"""
-page = requests.get('http://calendar.artsci.utoronto.ca/crs_mat.htm')
+page = requests.get('https://fas.calendar.utoronto.ca/print/search-courses-print?')
 tree = html.fromstring(page.content)
 
 # Create a list of course names
-course_names = tree.xpath('//span[@class="strong"]/text()')
+course_titles = tree.xpath('//h3/text()')
+course_hours = tree.xpath('//span[@class="views-field views-field-field-hours"]/span[@class="field-content"]/text()')
 
 ### Print a list of the data
-print('Course Names: ', course_names)
+print('Course Names: ', course_titles)
+print('Hours: ', course_hours)
 
 # TODO: generate flowchart using pygraphviz
